73288c568e44c4904d1b4ea3903a015cb2292335
BOJREKAM <kanat.s13@gmail.com>
2024-05-08 13:25:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,151 @@
+const langData = {
+    'en': {
+        'home': 'Home',
+        'about': 'About',
+        'services': 'Services',
+        'contact': 'Contact Us',
+        'headerTitle': 'AIDATA',
+        'headerSubtitle': 'Advanced IT Architecture and Data Engineering Solutions',
+        'headerContact': 'Contact Us',
+        'dataIntegrationTitle': 'Data Integration',
+        'dataIntegrationDesc': 'Seamlessly merge data from multiple sources for comprehensive analysis and reporting. Our solutions are designed to streamline your data flows, enhance data accuracy, and provide a unified view that supports better business decisions and operational efficiency.',
+        'dataModelingTitle': 'Data Modeling',
+        'dataModelingDesc': 'Design and develop robust data models to efficiently organize and visualize your data. Our expert team ensures that each model is tailored to enhance data integrity and usability, empowering your business with clear insights and superior decision-making capabilities.',
+        'dataWarehousingTitle': 'Data Warehousing',
+        'dataWarehousingDesc': 'Build and maintain scalable data warehouses for secure and efficient data storage. We specialize in creating bespoke Data Warehousing (DWH) solutions from scratch, as well as data migrations that optimize budget resources and performance speed. Trust us to enhance your data capabilities with state-of-the-art storage solutions that meet your business needs.',
+        'dataQualityTitle': 'Data Quality Management',
+        'dataQualityDesc': 'Ensure the accuracy and reliability of your data to make informed decisions. Our specialized services help you maintain pristine data integrity, reduce errors, and increase trust in your analytics. By optimizing your data quality, we empower your business to operate more efficiently and make strategic decisions with confidence. Partner with us to transform your data into a reliable foundation for your success.',
+        'businessIntelligenceTitle': 'Business Intelligence',
+        'businessIntelligenceDesc': 'Unlock actionable insights and visualize data to drive strategic decision-making. Our analytics solutions empower you to interpret complex data, identify trends, and make informed choices that propel your business forward.',
+        'dataGovernanceTitle': 'Data Governance',
+        'dataGovernanceDesc': 'Establish robust governance policies and processes to ensure data integrity and compliance. Our data management services are designed to secure your data accuracy and adherence to regulatory standards, providing a strong foundation for all your business operations. Trust our experts to implement the frameworks that will protect your data and support your strategic goals. Partner with us to safeguard your information assets and maintain the highest standards of data excellence.'
+    },
+    'ru': {
+        'home': 'Главная',
+        'about': 'О нас',
+        'services': 'Услуги',
+        'contact': 'Контакты',
+        'headerTitle': 'AIDATA',
+        'headerSubtitle': 'Продвинутая IT архитектура и решения в области данных',
+        'headerContact': 'Связаться с нами',
+        'dataIntegrationTitle': 'Интеграция данных',
+        'dataIntegrationDesc': 'Оптимальная интеграция данных из различных источников для всестороннего анализа и составления отчетов. Наши решения нацелены на оптимизацию потоков данных, повышение точности информации и предоставление единой картины, которая способствует принятию обоснованных бизнес-решений и повышению операционной эффективности.',
+        'dataModelingTitle': 'Моделирование данных',
+        'dataModelingDesc': 'Проектируем и создаем надежные модели данных для эффективной организации и визуализации информации. Наша команда экспертов гарантирует, что каждая модель адаптирована для повышения целостности и удобства использования данных, предоставляя вашему бизнесу четкие инсайты и улучшенные возможности принятия решений.',
+        'dataWarehousingTitle': 'Хранилище данных',
+        'dataWarehousingDesc': 'Создаем и поддерживаем масштабируемые хранилища данных для безопасного и эффективного хранения информации. Мы специализируемся на разработке индивидуальных решений для хранилищ данных (DWH) с нуля, а также на миграции данных для оптимизации бюджетных средств и ускорения работы. Доверьтесь нам в вопросах улучшения ваших возможностей по обработке данных с помощью передовых решений для хранения, соответствующих потребностям вашего бизнеса.',
+        'dataQualityTitle': 'Управление качеством данных',
+        'dataQualityDesc': 'Обеспечем точность и надежность ваших данных для принятия обоснованных решений. Наши специализированные услуги помогут вам поддерживать идеальную целостность данных, снижать количество ошибок и повышать доверие к вашей аналитике. Оптимизируя качество данных, мы даем вашему бизнесу возможность работать более эффективно и принимать стратегические решения с уверенностью. Сотрудничайте с нами, чтобы превратить ваши данные в надежную основу вашего успеха.',
+        'businessIntelligenceTitle': 'Бизнес-аналитика',
+        'businessIntelligenceDesc': 'Получайте полезные инсайты и визуализируйте данные для стратегического принятия решений. Наши решения в области аналитики позволяют вам анализировать сложные данные, выявлять тенденции и принимать обоснованные решения, которые стимулируют развитие вашего бизнеса.',
+        'dataGovernanceTitle': 'Управление данными',
+        'dataGovernanceDesc': 'Установите надежные политики и процессы управления для обеспечения целостности и соответствия данных. Наши услуги по управлению данными разработаны для обеспечения точности ваших данных и соблюдения нормативных стандартов, создавая крепкую основу для всех ваших бизнес-операций. Доверьтесь нашим экспертам в реализации систем, которые защитят ваши данные и поддержат ваши стратегические цели. Сотрудничайте с нами для защиты ваших информационных активов и поддержания высочайших стандартов качества данных.'
+    },
+    'kz': {
+        'home': 'Басты',
+        'about': 'Біз туралы',
+        'services': 'Қызметтер',
+        'contact': 'Байланыс',
+        'headerTitle': 'AIDATA',
+        'headerSubtitle': 'Ақпараттық технологиялардың және деректер инжинирингінің озық шешімдері',
+        'headerContact': 'Байланысу',
+        'dataIntegrationTitle': 'Data Integration',
+        'dataIntegrationDesc': 'Жан-жақты талдау және есеп беру үшін әртүрлі көздерден алынған деректерді оңтайлы біріктіру. Біздің шешімдеріміз деректер ағындарын оңтайландыруға, ақпарат дәлдігін жақсартуға және негізделген бизнес шешімдерін қабылдауға және операциялық тиімділікті арттыруға көмектесетін бір көріністі қамтамасыз етуге арналған.',
+        'dataModelingTitle': 'Data Modeling',
+        'dataModelingDesc': 'Біз ақпаратты тиімді ұйымдастыру және визуализациялау үшін сенімді деректер үлгілерін әзірлеп, жасаймыз. Біздің сарапшылар тобы бизнесіңізге нақты түсініктер мен жақсартылған шешім қабылдау мүмкіндіктерін бере отырып, деректер тұтастығы мен ыңғайлылығын жақсарту үшін әрбір үлгінің бейімделуін қамтамасыз етеді.',
+        'dataWarehousingTitle': 'Data Warehousing',
+        'dataWarehousingDesc': 'Біз ақпаратты қауіпсіз және тиімді сақтау үшін масштабталатын деректер қоймаларын жасаймыз және қолдаймыз. Біз бастапқы деректер қоймасы (DWH) шешімдерін әзірлеуге, сондай-ақ бюджеттерді оңтайландыру және операцияларды жылдамдату үшін деректерді тасымалдауға маманданамыз. Бизнес қажеттіліктеріңізді қанағаттандыратын кеңейтілген сақтау шешімдерімен деректерді өңдеу мүмкіндіктерін жақсарту үшін бізге сеніңіз.',
+        'dataQualityTitle': 'Data Quality Management',
+        'dataQualityDesc': 'Саналы шешім қабылдау үшін деректеріңіздің дәл және сенімді екеніне көз жеткізіңіз. Арнайы қызметтеріміз деректердің мінсіз тұтастығын сақтауға, қателерді азайтуға және талдауларыңызға сенімділікті арттыруға көмектеседі. Деректер сапасын оңтайландыру арқылы біз сіздің бизнесіңізге тиімдірек жұмыс істеуге және стратегиялық шешімдерді сенімді қабылдауға мүмкіндік береміз. Деректеріңізді табысыңыздың берік негізіне айналдыру үшін бізбен серіктес болыңыз.',
+        'businessIntelligenceTitle': 'Business Intelligence',
+        'businessIntelligenceDesc': 'Стратегиялық шешімдерді қабылдау үшін әрекетке болатын түсініктерді алыңыз және деректерді визуализациялаңыз. Біздің аналитикалық шешімдеріміз күрделі деректерді талдауға, трендтерді анықтауға және бизнесіңізді алға жылжытатын негізделген шешімдер қабылдауға мүмкіндік береді.',
+        'dataGovernanceTitle': 'Data Governance',
+        'dataGovernanceDesc': 'Деректердің тұтастығы мен сәйкестігін қамтамасыз ету үшін күшті басқару саясаты мен процестерін құрыңыз. Біздің деректерді басқару қызметтеріміз деректеріңіздің дәлдігін және реттеуші стандарттарға сай болуын қамтамасыз ету үшін жасалған, бұл сіздің барлық бизнес операцияларыңыз үшін мықты негіз жасайды. Деректеріңізді қорғайтын және стратегиялық мақсаттарыңызды қолдайтын жүйелерді енгізу үшін біздің мамандарға сеніңіз. Ақпараттық активтеріңізді қорғау және деректер сапасының ең жоғары стандарттарын сақтау үшін бізбен серіктес болыңыз.'
+    }
+};
+
+
+document.querySelector('.lang-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    const dropdown = document.querySelector('.languages-dropdown');
+    dropdown.classList.toggle('hidden'); // Toggle visibility
+});
+
+function setLanguage(lang) {
+    document.querySelectorAll('[data-key]').forEach(elem => {
+        elem.textContent = langData[lang][elem.getAttribute('data-key')];
+    });
+
+    history.pushState({lang: lang}, '', '?lang=' + lang);
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const offset = 70; // Height of the navbar, adjust as needed
+
+    document.querySelectorAll('.nav-links a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            let target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop - offset,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+
+    var telegramLink = document.getElementById('telegramLink');
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        // Mobile device detected
+        telegramLink.href = "tg://resolve?domain=@syrym123";
+    } else {
+        // Desktop detected
+        telegramLink.href = "https://web.telegram.org/k/#@syrym123";
+    }
+});
+
+
+const countdownDate = new Date("May 31, 2024 00:00:00").getTime();
+
+const x = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countdownDate - now;
+
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("hours").innerText = hours < 10 ? '0' + hours : hours;
+    document.getElementById("minutes").innerText = minutes < 10 ? '0' + minutes : minutes;
+    document.getElementById("seconds").innerText = seconds < 10 ? '0' + seconds : seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+
+const syncPointer = ({ x: pointerX, y: pointerY }) => {
+    const x = pointerX.toFixed(2)
+    const y = pointerY.toFixed(2)
+    const xp = (pointerX / window.innerWidth).toFixed(2)
+    const yp = (pointerY / window.innerHeight).toFixed(2)
+    document.documentElement.style.setProperty('--x', x)
+    document.documentElement.style.setProperty('--xp', xp)
+    document.documentElement.style.setProperty('--y', y)
+    document.documentElement.style.setProperty('--yp', yp)
+  }
+  document.body.addEventListener('pointermove', syncPointer)
+
+
+
+function openWhatsApp() {
+    var waMessage = encodeURIComponent("Want to roadmap");
+    var waUrl = "https://wa.me/+77714438894?text=" + waMessage;
+    window.open(waUrl, '_blank');
+}
